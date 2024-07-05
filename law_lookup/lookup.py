23dae508af53d2c2fc0817c101034bcd01916428
spehnm <spehmaxi@gmail.com>
2024-07-05 15:02:43 +0200

    Order and complete lookup.py methods

--- law_lookup/lookup.py
+++ law_lookup/lookup.py
@@ -2,24 +2,48 @@
 
 import re
 import webbrowser
-import tokenizer_german_legal_jargon
+import tokenizer_german_legal_jargon as tok
 from aqt.reviewer import Reviewer
 from anki.hooks import wrap
 
 class LawLookup:
     def __init__(self):
-        pass  # Add suitable regex
+        self.install_hooks()
     
-    def get_expression_slices(self, text):
-        pass  # Slice expression so we can insert strings into URL
+    def get_text_on_front_card(self, card):
+        return card.q()
     
-    def get_reference(self, text):
-        expression_slices = self.get_expression_slices()        
-        # Insert slices into url and open the url
+    def tokenize_front_card(self, card):
+        text = self.get_text_on_front_card(card)
+        return tok.tokenizer_german_legal_texts(text)
     
-    def on_card_show(self, card):
-        front_card = card.q()
-        self.get_reference(front_card)
+    def get_first_reference(self, tokens):
+        for token in tokens:
+            if token.startswith('§'):
+                return token
+        return None  # If no token starts with '§' we do not have any reference
+    
+    def get_expression_slices(self, first_reference):
+        parts = first_reference.split()
+        if len(parts) >= 3:
+            section_number = parts[1]  # Gets '1' from '§ 1 BGB'
+            law = parts[-1]  # Gets 'BGB' from '§ 1 BGB'
+            return section_number, law
+        return None, None
+            
+        
+    def get_reference(self, section_number, law):
+        url = f"https://www.gesetze-im-internet.de/{law}/__{section_number}.html"
+        webbrowser.open(url)
+    
+    def on_card_show(self, card, _old):
+        tokens = self.tokenize_front_card(card)
+        first_reference = self.get_first_reference(tokens)
+        if first_reference:
+            section_number, law = self.get_expression_slices(first_reference)
+            if section_number and law:
+                self.get_reference(section_number, law)
+
         
     def install_hooks(self):
         Reviewer._showQuestion = wrap(Reviewer._showQuestion, self._on_reviewer_show_question, "after")
